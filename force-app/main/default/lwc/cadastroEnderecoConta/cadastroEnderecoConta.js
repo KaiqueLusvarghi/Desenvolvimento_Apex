d6eb5a35c68a18bba36b14fa7b3c020261dcc0d6
KaiqueLusvarghi <kaiqueolusvarghi@gmail.com>
2026-04-06 15:10:00 -0300

    Salvando Cep

--- force-app/main/default/lwc/cadastroEnderecoConta/cadastroEnderecoConta.js
+++ force-app/main/default/lwc/cadastroEnderecoConta/cadastroEnderecoConta.js
@@ -1,25 +1,58 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import buscerCep from '@salesforce/apex/CadastroEnderecoContaController.buscerCep';
+import salvarEndereco from '@salesforce/apex/CadastroEnderecoContaController.salvarEndereco';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class CadastroEnderecoConta extends LightningElement {
-
+endereco;
 cep;
+@api recordId;
+
+    connectedCallback() {
+    console.log(this.recordId);    
+    }
 
     handleCepChange(event) {
-        this.cep = event.target.value;
+        this.cep = event.detail.value;
         
     }
 
     handleBuscarCep() {
-        buscerCep({ cep: this.cep })
-            .then(endereco => {
-                console.log(JSON.stringify(endereco));
-            })
-            .catch(error => {
-                console.error('Erro ao buscar CEP: ' + error);
+        buscerCep({ cep:this.cep })
+            .then((endereco) => {
+                this.endereco = endereco;
+                this.ShowToastEvent('Sucesso', 'Endereço encontrado com sucesso!', 'success');
+            }).catch((error) => {
+                this.ShowToastEvent('Error!', 'Não foi possível encontrar o endereço para o CEP informado.', 'error');
             });
+            
+    }
+    handleChangeEndereco(event) {
+    const field = event.target.dataset.field;
+    const value = event.detail.value;                        
+    this.endereco = { ...this.endereco, [field]: value };    
+}
 
+    handleSalvarEndereco(){
+        salvarEndereco({ 
+            contaId: this.recordId, 
+            endereco: JSON.stringify(this.endereco) 
+        }).then(() => {
+            this.ShowToastEvent('Sucesso', 'CEP Salvo com Sucesso!', 'success');
+        }).catch((error) => {
+            this.ShowToastEvent('Error!', 'Erro ao Salvar o Endereço.',error.body.message,'error');
+        });
 
+    }
+
+    ShowToastEvent(title, message, variant) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: title,
+                message: message,
+                variant: variant
+            })
+        );
     }
 }
 
